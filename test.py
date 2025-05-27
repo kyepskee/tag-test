0ee9a215268c92087b2ca0b30ba22abf7f28713c
kyepskee <hubi.wasilewski@gmail.com>
2025-05-27 23:15:18 +0200

    fix: run argument order

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ def compile():
         args=["g++", *FLAGS, "touchk.cpp", "-o", "checker"]
     )
 
-def run(test_in: str, test_out: str, user_out: str):
+def run(test_in: str, user_out: str, test_out: str):
     test_in_file = tempfile.NamedTemporaryFile(delete=False)
     test_out_file = tempfile.NamedTemporaryFile(delete=False)
     user_out_file = tempfile.NamedTemporaryFile(delete=False)
@@ -31,48 +31,50 @@ def run(test_in: str, test_out: str, user_out: str):
     for f in [test_in_file, test_out_file, user_out_file]:
         f.close()
 
-    ret = subprocess.run(["./checker", test_in_file.name, test_out_file.name, user_out_file.name], capture_output=True)
+    ret = subprocess.run(
+        ["./checker", test_in_file.name, user_out_file.name, test_out_file.name],
+        # env={'USER': 'oioioiworker'},
+        capture_output=True)
 
     return ret
 
 class TestCorrect():
     inputs = [("1\n2 1\n1 2 1", "NO", "NO")]
 
-    @pytest.mark.parametrize("test_in,test_out,user_out", inputs)
+    @pytest.mark.parametrize("test_in,user_out,test_out", inputs)
     def test_correct(self, compile, test_in, test_out, user_out):
         assert run(test_in, test_out, user_out).stdout == b'OK\n\n100\n'
 
 class TestWrong():
     inputs = [("1\n2 1\n1 2 1", "NO", "no"), ("1\n2 1\n1 2 1", "NO", "YES\n1 1")]
 
-    @pytest.mark.parametrize("test_in,test_out,user_out", inputs)
+    @pytest.mark.parametrize("test_in,user_out,test_out", inputs)
     def test_wrong(self, compile, test_in, test_out, user_out):
         assert run(test_in, test_out, user_out).stdout == b'WRONG\n\n0\n'
         
 def wrong_message(line, pos, message, score = 0):
-    return f"WRONG\nWiersz {line}, pozycja {pos}: {message}"
+    return bytes(f"WRONG\nWiersz {line}, pozycja {pos}: {message}\n{score}\n", encoding='utf8')
 
 def wrong_out_of_range(line, pos, score = 0):
     return wrong_message(line, pos, "Liczba calkowita spoza zakresu") 
 
 def wrong_expected(line, pos, char, expected, score = 0):
-    return wrong_message(line, pos, "Wczytano '{char}', oczekiwano {expected}")
+    return wrong_message(line, pos, f"Wczytano '{char}', oczekiwano {expected}")
 
 class TestVerbatim():
     inputs = [
             (
-"""1
-3 4
+"""2
+2 1
 1 2 1
-2 3 2
-3 2 3
-2 1 1""",
-                "YES\n2 2 3",
-                "\nYES\n2 3 2",
-                wrong_expected(1, 1, "\\n", "napisu")
+2 1
+1 2 1""",
+                "NO\nNO",
+                "NO\n\nNO",
+                wrong_expected(2, 1, "\\n", "napisu")
             )]
 
-    @pytest.mark.parametrize("test_in,test_out,user_out,checker_out", inputs)
+    @pytest.mark.parametrize("test_in,user_out,test_out,checker_out", inputs)
     def test_verbatim(self, compile, test_in, test_out, user_out, checker_out):
         assert run(test_in, test_out, user_out).stdout == checker_out
 
